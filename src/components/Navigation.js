b63c09833409bf65533b6c0114ec0c944ed5c83b
mi <whymi627@gmil.com>
2024-03-04 12:27:02 +0900

    chap 07_1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+// 라우터 내에서 직접적으로 페이지 이동을 하고자 할때 사용하는 컴포넌트
 
 const Navigation = () => {
     return (
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,13 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { collection, query, where, orderBy, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Nweet from "components/Nweet";
 
-const Profile = () => {
+// 로그아웃 signOut() 이용!! 
+const Profile = ({ userObj }) => {
+
+    const [nweets, setNweets] = useState([]);
 
     const navigate = useNavigate();
 
@@ -10,9 +16,39 @@ const Profile = () => {
         navigate("/")
     };
 
+    const getMyNweets = async () => {
+        // const nweets = await collection(dbService, "nweets");
+        const q = query(
+            collection(dbService, "nweets"),
+            where("creatorId", "==" , userObj.uid),
+            orderBy("createdAt", "asc")
+        );
+
+        const querySnapshot = await getDocs(q);
+        const nweets = [];
+        querySnapshot.forEach((doc) => {
+            nweets.push(doc.data());
+        });
+        console.log(nweets);
+        setNweets(nweets);
+    }
+
+    useEffect( () => {
+        getMyNweets();
+    }, []);
+
     return (
         <>
             <button onClick={onLogOutClick}>Log Out</button>
+            <div>
+                {nweets.map((nweet) => (
+                    <Nweet 
+                        key={nweet.id} 
+                        nweetObj={nweet}
+                        isOwner={nweet.creatorId === userObj.uid}
+                    />
+                ))}
+            </div>
         </>
     );
 
